efae91824a21f7d077a43171155d898d36b5c030
rahullepcha <rahulmaxlepcha@gmail.com>
2020-09-13 00:10:01 +0530

    successfully landed Static page

--- frontend/src/main/java/com/law/frontend/controller/PageController.java
+++ frontend/src/main/java/com/law/frontend/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.law.frontend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,4 +15,25 @@ public class PageController {
 			mv.addObject("greeting","Welcome To Law Appointment");
 			return mv;
 		}
+//
+//		@RequestMapping(value="/test")
+//		public ModelAndView test(@RequestParam(value="greeting",required=false)String greeting) {
+//			if(greeting==null) {
+//				greeting="hello there";
+//			}
+//			ModelAndView mv=new ModelAndView("page");
+//			mv.addObject("greeting","Welcome To Law Appointment");
+//			return mv;
+//		}
+
+		@RequestMapping(value="/test/{greeting}")
+		public ModelAndView test(@PathVariable("greeting")String greeting) {
+			if(greeting==null) {
+				greeting="hello there";
+			}
+			ModelAndView mv=new ModelAndView("page");
+			mv.addObject("greeting",greeting);
+			return mv;
+		}
+
 }
